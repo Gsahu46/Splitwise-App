e0fac0f3553dd4b35d0ff0b3ea5e65cb4c0f371d
Gaurav sahu <gauravsahucs18@aryacollege.in>
2021-09-16 20:30:24 +0530

    my project

---
+++ cmake-build-debug/CMakeFiles/Splitwise__.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Splitwise__.dir/main.cpp.obj"
+  "Splitwise__.exe"
+  "Splitwise__.exe.manifest"
+  "Splitwise__.pdb"
+  "libSplitwise__.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Splitwise__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
